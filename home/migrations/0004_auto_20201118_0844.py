947a21007fdf34563512206e382e9ba1c83d6d7a
Crowdbotics <team@crowdbotics.com>
2020-11-18 08:45:52 +0000

    Update models

--- home/migrations/0004_auto_20201118_0844.py
+++ home/migrations/0004_auto_20201118_0844.py
@@ -6,18 +6,18 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0003_auto_20201118_0838'),
+        ("home", "0003_auto_20201118_0838"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='customtext',
-            old_name='hgjghjhjgjgjh',
-            new_name='title',
+            model_name="customtext",
+            old_name="hgjghjhjgjgjh",
+            new_name="title",
         ),
         migrations.RenameField(
-            model_name='homepage',
-            old_name='hgfhfgfhgfhgfhgf',
-            new_name='body',
+            model_name="homepage",
+            old_name="hgfhfgfhgfhgfhgf",
+            new_name="body",
         ),
     ]
